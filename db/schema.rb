fb62b0f0be960afabb26ccd8fe3ee0833d517788
Dave Carlson <davicar319@acm.org>
2015-11-07 06:20:26 -0600

    added person_info and jobs tables to person.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101122304) do
+ActiveRecord::Schema.define(version: 20151107121822) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "company"
+    t.string   "position_id"
+    t.integer  "person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "jobs", ["person_id"], name: "index_jobs_on_person_id"
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
@@ -22,5 +33,15 @@ ActiveRecord::Schema.define(version: 20151101122304) do
     t.string   "login"
     t.string   "pass"
   end
+
+  create_table "personal_infos", force: :cascade do |t|
+    t.float    "height"
+    t.float    "weight"
+    t.integer  "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "personal_infos", ["person_id"], name: "index_personal_infos_on_person_id"
 
 end
